fb56a2c0b99533fd16efed1675ac410c117a8283
pjhuang <506750334@qq.com>
2021-02-19 18:01:16 +0800

    工厂模式

--- studyTS/DesignModel.ts
+++ studyTS/DesignModel.ts
@@ -1,14 +1,40 @@
-namespace singleton{
+//https://github.com/youlookwhat/DesignPattern
+//https://www.yiibai.com/design_pattern/abstract_factory_pattern.html
+//传入类名
+class A {
+}
+function fun(cls: new () => A) {
+}
+fun(A)
+
+//extend 与 implement 区别
+/**
+ * extend是继承，只能继承一个类
+ * implement是实现多个接口
+ * class A extends B implements C,D,E   
+ */
+
+/**
+ * interface 和 abstract
+ * interface 没有访问修饰符，没有函数体,用implements。interface 就是特效的abstract
+ * abstract 可以抽象成员和方法，抽象方法没有函数体，子类必须实现
+ */
+
+//ts删除可以用 null
+//删除对象属性可以用delete
+
+//单列模式
+namespace singleton {
     //优点共享资源
     //缺点无法继承
-    class Toast{
+    class Toast {
         //静态私有的成员
-        private static instance:Toast;
+        private static instance: Toast;
         //构造函数私有
-        private constructor(){};
+        private constructor() { };
         //提供一个静态访问方法
-        public static getInstance():Toast{
-            if(!this.instance){
+        public static getInstance(): Toast {
+            if (!this.instance) {
                 Toast.instance = new Toast();
             }
             return Toast.instance;
@@ -17,61 +43,295 @@ namespace singleton{
 
 }
 
-namespace observer{
+
+//=============================================//
+/**
+优点： 1、观察者和被观察者是抽象耦合的。 2、建立一套触发机制。
+缺点： 1、如果一个被观察者对象有很多的直接和间接的观察者的话，将所有的观察者都通知到会花费很多时间。观察目标会触发它们之间进行循环调用，可能导致系统崩溃。
+ */
+//监视者模式
+namespace observer {
 
     //主题接口，所有主题必须实现此接口
-    interface Subject{
+    interface Subject {
 
         //注册一个观察者
-        registerObserver(o:Observer):void;
+        registerObserver(o: Observer): void;
 
         //移除一个观察者
-        removeObservr(o:Observer):void;
+        removeObservr(o: Observer): void;
 
         //通知所有的观察者
-        notifyObserver():void;
+        notifyObserver(): void;
     }
 
     //所有观察者接口
-    interface Observer{
-        update(msg:string):void;
+    interface Observer {
+        update(msg: string): void;
     }
 
-    class ObjectFor3D implements Subject{
+    class ObjectFor3D implements Subject {
 
-        private observerList:Observer[] = [];
+        private observerList: Observer[] = [];
 
-        private msg:string;
+        private msg: string;
 
-        registerObserver(o:Observer) :void{
+        public registerObserver(o: Observer): void {
             this.observerList.push(o);
         }
 
-        removeObservr(o:Observer):void{
-            this.observerList.indexOf(o);
+        public removeObservr(o: Observer): void {
+            let index = this.observerList.indexOf(o);
+            if (index > -1) {
+                this.observerList.splice(index, 1);
+            }
         }
 
-        notifyObserver():void{
-
+        public notifyObserver(): void {
+            for (let i = 0; i < this.observerList.length; i++) {
+                const element = this.observerList[i];
+                element.update(this.msg);
+            }
         }
 
-
-
+        //设置更新消息
+        public setMsg(msg: string): void {
+            this.msg = msg;
+            this.notifyObserver();
+        }
     }
+
+    class Observer1 implements Observer {
+
+        private Subject: Subject;
+
+        constructor(Subject: Subject) {
+            this.Subject = Subject;
+            this.Subject.registerObserver(this);
+        }
+
+        public update(msg: string): void {
+            console.log('Observer1msg ==============', msg);
+        }
+    }
+
+    class Observer2 implements Observer {
+
+        private Subject: Subject;
+
+        constructor(Subject: Subject) {
+            this.Subject = Subject;
+            this.Subject.registerObserver(this);
+        }
+
+        public update(msg: string): void {
+            console.log('Observer2 msg ==============', msg);
+        }
+    }
+
+    let o: ObjectFor3D = new ObjectFor3D();
+
+    let ob1: Observer1 = new Observer1(o);
+    let ob2: Observer2 = new Observer2(o);
+
+    o.setMsg("监视者模式,传入消息")
 }
 
-class A{
+
+//==================================//
+
+//工厂模式
+namespace Factory {
+
+    // 1、静态工厂模式
+
+    // 这个最常见了，项目中的辅助类，TextUtil.isEmpty等，类+静态方法。下面开始详细介绍：略。
+
+    abstract class RouJiaMo {
+        protected _name: string;
+        //准备工作
+        public prepare(): void {
+            console.log('揉面-剁肉-完成准备工作');
+        }
+        //使用你们的专用袋-包装
+        public pack(): void {
+            console.log('肉夹馍-专用袋-包装');
+        }
+        //秘制设备-烘烤2分钟
+        public fire(): void {
+            console.log('若夹馍·专用设备-烘烤');
+        }
+    }
+
+    //酸味肉夹馍
+    class SuanRouJiaMo extends RouJiaMo {
+        constructor() {
+            super();
+            this._name = "酸味肉夹馍";
+        }
+    }
+
+    //辣味肉夹馍
+    class LaRouJiaMo extends RouJiaMo {
+        constructor() {
+            super();
+            this._name = "辣味肉夹馍";
+        }
+    }
+
+    //简单工厂
+    namespace Factory.a {
+        class SimpleRouJiaMoFactory {
+            public createRouJiaMo(type: string): RouJiaMo {
+                let roujiamo: RouJiaMo = null;
+                if (type == "Suan") {
+                    roujiamo = new SuanRouJiaMo();
+                } else if (type == "La") {
+                    roujiamo = new LaRouJiaMo();
+                }
+                return roujiamo;
+            }
+        }
+
+        class RoujiamoStore {
+            private factory: SimpleRouJiaMoFactory
+            constructor(factory: SimpleRouJiaMoFactory) {
+                this.factory = factory;
+            }
+
+            //根据传入类型卖不同的肉夹馍
+            public sellRouJiaMo(type: string): RouJiaMo {
+                let roujiamo: RouJiaMo = this.factory.createRouJiaMo(type);
+                roujiamo.prepare();
+                roujiamo.fire();
+                roujiamo.pack()
+                return roujiamo
+            }
+        }
+    }
+
+    //工厂方法
+    namespace Factory.b {
+
+        abstract class RoujiaMoStore {
+
+            abstract createRouJiaMo(type: string)
+
+            //根据传入类型卖不同的肉夹馍
+            public sellRouJiaMo(type: string): RouJiaMo {
+                let roujiamo: RouJiaMo = this.createRouJiaMo(type);
+                roujiamo.prepare();
+                roujiamo.fire();
+                roujiamo.pack()
+                return roujiamo
+            }
+        }
+
+        class XianRouJiaMoStore extends RoujiaMoStore {
+            createRouJiaMo(type: string) {
+                let roujiamo: RouJiaMo = null;
+                if (type == "Suan") {
+                    roujiamo = new SuanRouJiaMo();
+                } else if (type == "La") {
+                    roujiamo = new LaRouJiaMo();
+                }
+                return roujiamo;
+            }
+        }
+    }
+
+    //抽象工厂模式
+    namespace Factory.c {
+        interface Shape {
+            draw(): void;
+        }
+
+        class Rect implements Shape{
+            draw(){
+                console.log('draw Rect');
+            }
+        }
+
+        class Square implements Shape{
+            draw(){
+                console.log('draw Square');
+            }
+        }
+
+        interface Color{
+            fill():void;
+        }
+
+        class Red implements Color{
+            fill(){
+                console.log('color red');
+            }
+        }
+
+        class Greed implements Color {
+            fill(){
+                console.log('color greed');
+            }
+        }
+
+
+        abstract class AbstractFactory{
+            abstract getColor(color:string):Color;
+            abstract getDraw(shap:string):Shape;
+        }
+        
+        class shapeFactory extends AbstractFactory{
+            getColor(color:string):Color{
+                return null;
+            }
+
+            getDraw(shap:string):Shape{
+                if(shap == null){
+                    return null;
+                }
+
+                if(shap == "rect"){
+                    return new Rect();
+                }else if(shap == "square"){
+                    return new Square();
+                }
+            }
+        }
+
+        class ColorFactory extends AbstractFactory {
+            getColor(color:string):Color{
+                if(color == null){
+                    return null;
+                }
+
+                if(color == "red"){
+                    return new Red();
+                }else if(color == "greed"){
+                    return new Greed();
+                }
+            }
+
+            getDraw(shap:string):Shape{
+                return null;
+            }
+        }
+
+
+        class FactoryProducer{
+            public static getFactory(choice:string):AbstractFactory{
+                if(choice == "SHAPE"){
+                    return new shapeFactory();
+                }else if(choice == "COLOR"){
+                    return new ColorFactory();
+                }
+                return null;
+            }
+        }
+
+        let f1 = FactoryProducer.getFactory("SHAPE");
+        let shap:Shape = f1.getDraw("rect");
+        shap.draw();
+    }
 
 }
 
-let aList:A[] = [];
-let a1 = new A();
-let a2 = new A();
-
-aList.push(a1);
-aList.push(a2);
-
-console.log(aList.indexOf(a2));
- 
-//https://blog.csdn.net/lmj623565791/article/details/24179699
-//https://github.com/youlookwhat/DesignPattern
--- studyTS/dist/DesignModel.js
+++ studyTS/dist/DesignModel.js
@@ -1,3 +1,41 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+//https://github.com/youlookwhat/DesignPattern
+//https://www.yiibai.com/design_pattern/abstract_factory_pattern.html
+//传入类名
+var A = /** @class */ (function () {
+    function A() {
+    }
+    return A;
+}());
+function fun(cls) {
+}
+fun(A);
+//extend 与 implement 区别
+/**
+ * extend是继承，只能继承一个类
+ * implement是实现多个接口
+ * class A extends B implements C,D,E
+ */
+/**
+ * interface 和 abstract
+ * interface 没有访问修饰符，没有函数体,用implements。interface 就是特效的abstract
+ * abstract 可以抽象成员和方法，抽象方法没有函数体，子类必须实现
+ */
+//ts删除可以用 null
+//删除对象属性可以用delete
+//单列模式
 var singleton;
 (function (singleton) {
     //优点共享资源
@@ -17,6 +55,12 @@ var singleton;
         return Toast;
     }());
 })(singleton || (singleton = {}));
+//=============================================//
+/**
+优点： 1、观察者和被观察者是抽象耦合的。 2、建立一套触发机制。
+缺点： 1、如果一个被观察者对象有很多的直接和间接的观察者的话，将所有的观察者都通知到会花费很多时间。观察目标会触发它们之间进行循环调用，可能导致系统崩溃。
+ */
+//监视者模式
 var observer;
 (function (observer) {
     var ObjectFor3D = /** @class */ (function () {
@@ -27,22 +71,265 @@ var observer;
             this.observerList.push(o);
         };
         ObjectFor3D.prototype.removeObservr = function (o) {
-            this.observerList.indexOf(o);
+            var index = this.observerList.indexOf(o);
+            if (index > -1) {
+                this.observerList.splice(index, 1);
+            }
         };
         ObjectFor3D.prototype.notifyObserver = function () {
+            for (var i = 0; i < this.observerList.length; i++) {
+                var element = this.observerList[i];
+                element.update(this.msg);
+            }
+        };
+        //设置更新消息
+        ObjectFor3D.prototype.setMsg = function (msg) {
+            this.msg = msg;
+            this.notifyObserver();
         };
         return ObjectFor3D;
     }());
+    var Observer1 = /** @class */ (function () {
+        function Observer1(Subject) {
+            this.Subject = Subject;
+            this.Subject.registerObserver(this);
+        }
+        Observer1.prototype.update = function (msg) {
+            console.log('Observer1msg ==============', msg);
+        };
+        return Observer1;
+    }());
+    var Observer2 = /** @class */ (function () {
+        function Observer2(Subject) {
+            this.Subject = Subject;
+            this.Subject.registerObserver(this);
+        }
+        Observer2.prototype.update = function (msg) {
+            console.log('Observer2 msg ==============', msg);
+        };
+        return Observer2;
+    }());
+    var o = new ObjectFor3D();
+    var ob1 = new Observer1(o);
+    var ob2 = new Observer2(o);
+    o.setMsg("监视者模式,传入消息");
 })(observer || (observer = {}));
-var A = /** @class */ (function () {
-    function A() {
-    }
-    return A;
-}());
-var aList = [];
-var a1 = new A();
-var a2 = new A();
-aList.push(a1);
-aList.push(a2);
-console.log(aList.indexOf(a1));
+//==================================//
+//工厂模式
+var Factory;
+(function (Factory_1) {
+    // 1、静态工厂模式
+    // 这个最常见了，项目中的辅助类，TextUtil.isEmpty等，类+静态方法。下面开始详细介绍：略。
+    var RouJiaMo = /** @class */ (function () {
+        function RouJiaMo() {
+        }
+        //准备工作
+        RouJiaMo.prototype.prepare = function () {
+            console.log('揉面-剁肉-完成准备工作');
+        };
+        //使用你们的专用袋-包装
+        RouJiaMo.prototype.pack = function () {
+            console.log('肉夹馍-专用袋-包装');
+        };
+        //秘制设备-烘烤2分钟
+        RouJiaMo.prototype.fire = function () {
+            console.log('若夹馍·专用设备-烘烤');
+        };
+        return RouJiaMo;
+    }());
+    //酸味肉夹馍
+    var SuanRouJiaMo = /** @class */ (function (_super) {
+        __extends(SuanRouJiaMo, _super);
+        function SuanRouJiaMo() {
+            var _this = _super.call(this) || this;
+            _this._name = "酸味肉夹馍";
+            return _this;
+        }
+        return SuanRouJiaMo;
+    }(RouJiaMo));
+    //辣味肉夹馍
+    var LaRouJiaMo = /** @class */ (function (_super) {
+        __extends(LaRouJiaMo, _super);
+        function LaRouJiaMo() {
+            var _this = _super.call(this) || this;
+            _this._name = "辣味肉夹馍";
+            return _this;
+        }
+        return LaRouJiaMo;
+    }(RouJiaMo));
+    //简单工厂
+    var Factory;
+    (function (Factory) {
+        var a;
+        (function (a) {
+            var SimpleRouJiaMoFactory = /** @class */ (function () {
+                function SimpleRouJiaMoFactory() {
+                }
+                SimpleRouJiaMoFactory.prototype.createRouJiaMo = function (type) {
+                    var roujiamo = null;
+                    if (type == "Suan") {
+                        roujiamo = new SuanRouJiaMo();
+                    }
+                    else if (type == "La") {
+                        roujiamo = new LaRouJiaMo();
+                    }
+                    return roujiamo;
+                };
+                return SimpleRouJiaMoFactory;
+            }());
+            var RoujiamoStore = /** @class */ (function () {
+                function RoujiamoStore(factory) {
+                    this.factory = factory;
+                }
+                //根据传入类型卖不同的肉夹馍
+                RoujiamoStore.prototype.sellRouJiaMo = function (type) {
+                    var roujiamo = this.factory.createRouJiaMo(type);
+                    roujiamo.prepare();
+                    roujiamo.fire();
+                    roujiamo.pack();
+                    return roujiamo;
+                };
+                return RoujiamoStore;
+            }());
+        })(a = Factory.a || (Factory.a = {}));
+    })(Factory || (Factory = {}));
+    //工厂方法
+    (function (Factory) {
+        var b;
+        (function (b) {
+            var RoujiaMoStore = /** @class */ (function () {
+                function RoujiaMoStore() {
+                }
+                //根据传入类型卖不同的肉夹馍
+                RoujiaMoStore.prototype.sellRouJiaMo = function (type) {
+                    var roujiamo = this.createRouJiaMo(type);
+                    roujiamo.prepare();
+                    roujiamo.fire();
+                    roujiamo.pack();
+                    return roujiamo;
+                };
+                return RoujiaMoStore;
+            }());
+            var XianRouJiaMoStore = /** @class */ (function (_super) {
+                __extends(XianRouJiaMoStore, _super);
+                function XianRouJiaMoStore() {
+                    return _super !== null && _super.apply(this, arguments) || this;
+                }
+                XianRouJiaMoStore.prototype.createRouJiaMo = function (type) {
+                    var roujiamo = null;
+                    if (type == "Suan") {
+                        roujiamo = new SuanRouJiaMo();
+                    }
+                    else if (type == "La") {
+                        roujiamo = new LaRouJiaMo();
+                    }
+                    return roujiamo;
+                };
+                return XianRouJiaMoStore;
+            }(RoujiaMoStore));
+        })(b = Factory.b || (Factory.b = {}));
+    })(Factory || (Factory = {}));
+    //抽象工厂模式
+    (function (Factory) {
+        var c;
+        (function (c) {
+            var Rect = /** @class */ (function () {
+                function Rect() {
+                }
+                Rect.prototype.draw = function () {
+                    console.log('draw Rect');
+                };
+                return Rect;
+            }());
+            var Square = /** @class */ (function () {
+                function Square() {
+                }
+                Square.prototype.draw = function () {
+                    console.log('draw Square');
+                };
+                return Square;
+            }());
+            var Red = /** @class */ (function () {
+                function Red() {
+                }
+                Red.prototype.fill = function () {
+                    console.log('color red');
+                };
+                return Red;
+            }());
+            var Greed = /** @class */ (function () {
+                function Greed() {
+                }
+                Greed.prototype.fill = function () {
+                    console.log('color greed');
+                };
+                return Greed;
+            }());
+            var AbstractFactory = /** @class */ (function () {
+                function AbstractFactory() {
+                }
+                return AbstractFactory;
+            }());
+            var shapeFactory = /** @class */ (function (_super) {
+                __extends(shapeFactory, _super);
+                function shapeFactory() {
+                    return _super !== null && _super.apply(this, arguments) || this;
+                }
+                shapeFactory.prototype.getColor = function (color) {
+                    return null;
+                };
+                shapeFactory.prototype.getDraw = function (shap) {
+                    if (shap == null) {
+                        return null;
+                    }
+                    if (shap == "rect") {
+                        return new Rect();
+                    }
+                    else if (shap == "square") {
+                        return new Square();
+                    }
+                };
+                return shapeFactory;
+            }(AbstractFactory));
+            var ColorFactory = /** @class */ (function (_super) {
+                __extends(ColorFactory, _super);
+                function ColorFactory() {
+                    return _super !== null && _super.apply(this, arguments) || this;
+                }
+                ColorFactory.prototype.getColor = function (color) {
+                    if (color == null) {
+                        return null;
+                    }
+                    if (color == "red") {
+                        return new Red();
+                    }
+                    else if (color == "greed") {
+                        return new Greed();
+                    }
+                };
+                ColorFactory.prototype.getDraw = function (shap) {
+                    return null;
+                };
+                return ColorFactory;
+            }(AbstractFactory));
+            var FactoryProducer = /** @class */ (function () {
+                function FactoryProducer() {
+                }
+                FactoryProducer.getFactory = function (choice) {
+                    if (choice == "SHAPE") {
+                        return new shapeFactory();
+                    }
+                    else if (choice == "COLOR") {
+                        return new ColorFactory();
+                    }
+                    return null;
+                };
+                return FactoryProducer;
+            }());
+            var f1 = FactoryProducer.getFactory("SHAPE");
+            var shap = f1.getDraw("rect");
+            shap.draw();
+        })(c = Factory.c || (Factory.c = {}));
+    })(Factory || (Factory = {}));
+})(Factory || (Factory = {}));
 //# sourceMappingURL=DesignModel.js.map
